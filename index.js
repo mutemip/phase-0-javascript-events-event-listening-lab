f56d2000bf4adc8a35fde92638a427d761ebcd2a
mutemip <paulmutemi2030@gmail.com>
2022-06-25 00:15:28 +0300

    commit tree a40fde95f073a6bd109a5586fe3259b6b91fdc53

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+//const input = document.getElementById('input')//.addEventListener('click', () => alert('I was clicked!'))
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+    alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
